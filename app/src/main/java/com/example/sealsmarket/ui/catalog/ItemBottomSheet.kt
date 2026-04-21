9f767912525c1167197cbebadbc4f8ad88b31e5a
OficerKot <razyice@mail.ru>
2026-04-22 09:47:00 +1000

    Добавлен выбор размера товара в ItemBottomSheet.kt
    -Можно выбрать только один размер, который в будущем  при нажатии должен быть положен в корзину

--- app/src/main/java/com/example/sealsmarket/ui/catalog/ItemBottomSheet.kt
+++ app/src/main/java/com/example/sealsmarket/ui/catalog/ItemBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.example.sealsmarket.ui.catalog
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -23,12 +25,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -47,11 +54,17 @@ fun ItemInfoSheet(
     onClose: ()->Unit,
     modifier: Modifier = Modifier){
 
+    //Screen
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp.dp
+    val context = LocalContext.current
 
+    //Modal window
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val scope = rememberCoroutineScope()
+
+    //Size buttons
+    var selectedSize by remember{ mutableStateOf<String>(item.sizes[0]) }
 
     ModalBottomSheet(
         onDismissRequest = {
@@ -64,32 +77,39 @@ fun ItemInfoSheet(
         modifier = modifier){
         Box(
             modifier = Modifier
-                .height(screenHeight * 0.9f)
+                .height(screenHeight * 0.94f)
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .navigationBarsPadding()
         ) {
             Column() {
+
+                //Информация в верхней половине экрана
                 SheetItemInfo(item, modifier = Modifier.weight(1f))
 
+                //Информация в нижней половине экрана
                 Column(
                     verticalArrangement = Arrangement.Bottom,
                     modifier = Modifier
                         .padding(vertical = 16.dp)
                 ) {
+                    //Строка с размерами
                     LazyRow(
                         contentPadding = PaddingValues(bottom = 16.dp)
                     ) {
                         items(item.sizes) { size ->
                             SizeButton(
                                 size = size,
+                                onClick = {selectedSize=size},
+                                isSelected = (selectedSize==size),
                                 modifier = Modifier.padding(horizontal = 2.dp)
                             )
                         }
                     }
+                    //Кнопка добавления в корзину
                     Button(
                         onClick = {
                             /*Добавить в корзину*/
-
+                            Toast.makeText(context, "В корзину будет добавлен размер $selectedSize", Toast.LENGTH_SHORT).show()
                             scope.launch {
                                 sheetState.hide()
                             }.invokeOnCompletion {
@@ -109,6 +129,7 @@ fun ItemInfoSheet(
                     }
                 }
             }
+            //Теги
             LazyRow(modifier = Modifier
                 .align(Alignment.TopStart)) {
                 items(item.tags) { tag ->
@@ -171,10 +192,22 @@ fun SheetItemInfo(item:Item, modifier: Modifier = Modifier){
 @Composable
 fun SizeButton(
     size: String,
+    onClick: ()->Unit,
+    isSelected: Boolean,
     modifier: Modifier = Modifier
 ){
     Button(
-        onClick = {/*Выбрать этот размер*/},
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if(isSelected)
+                MaterialTheme.colorScheme.primary
+            else MaterialTheme.colorScheme.surfaceBright,
+
+            contentColor = if(isSelected)
+            MaterialTheme.colorScheme.onPrimary
+        else MaterialTheme.colorScheme.onSurface
+
+        ),
         shape = MaterialTheme.shapes.extraLarge,
         modifier = modifier){
         Text(text = size)
