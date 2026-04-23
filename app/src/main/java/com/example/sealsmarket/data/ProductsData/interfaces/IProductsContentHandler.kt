2afbda3231b12fc85aeb9444351e0ed0585fb054
Evgeniy <gooaal.j@mail.ru>
2026-04-23 17:09:53 +1000

    feat: добавление интерфейсов на будущее

---
+++ app/src/main/java/com/example/sealsmarket/data/ProductsData/interfaces/IProductsContentHandler.kt
@@ -0,0 +1,8 @@
+package com.example.sealsmarket.data.ProductsData.interfaces
+
+import com.example.sealsmarket.model.ProductsContent
+
+interface IProductsContentHandler : IProductsContentReciever, IProductsContentSetter
+{
+
+}
