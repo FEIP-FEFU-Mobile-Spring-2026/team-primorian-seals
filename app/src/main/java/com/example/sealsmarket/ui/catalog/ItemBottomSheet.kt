c42c31b98d5f315014c120fad5939a38b9070256
OficerKot <razyice@mail.ru>
2026-05-01 11:50:24 +1000

    update: в ItemBottomSheet.kt добавлены горизонтальные отступы у размеров

--- app/src/main/java/com/example/sealsmarket/ui/catalog/ItemBottomSheet.kt
+++ app/src/main/java/com/example/sealsmarket/ui/catalog/ItemBottomSheet.kt
@@ -96,7 +96,8 @@ fun ItemInfoSheet(
                 ) {
                     //Строка с размерами
                     LazyRow(
-                        contentPadding = PaddingValues(bottom = 16.dp)
+                        contentPadding = PaddingValues(bottom = 16.dp),
+                        modifier = Modifier.padding(horizontal = 16.dp )
                     ) {
                         items(item.sizes) { size ->
                             SizeButton(
